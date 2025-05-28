ad64ec2838b62464260a62a41358bf0ae5a3f0c7
JoschiZ <joschi.grey@posteo.de>
2025-05-28 21:15:27 +0200

    Initial Setup

---
+++ Cardpecker.Api/PeckerContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cardpecker.Api;
+
+public class PeckerContext : DbContext
+{
+    
+}
---
+++ Cardpecker.ServiceDefaults/AspireConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Cardpecker.ServiceDefaults;
+
+public static class AspireConstants
+{
+    public static class Databases
+    {
+        public const string Default = "Pecker";
+    }
+}
